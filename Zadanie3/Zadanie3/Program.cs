94ad3809c5808ecc397f0964ebedb397586130a1
EugeneTheBraixen <100828745+EugeneTheBraixen@users.noreply.github.com>
2023-10-09 14:22:46 +0300

    Add files via upload

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Интерфейс "Студент"
+public interface IStudent
+{
+    double CalculateAverageGrade();
+    int GetCourse();
+    string GetName();
+}
+
+// Класс "Студент первого курса"
+public class Freshman : IStudent
+{
+    private string name;
+    private double[] grades;
+    private int course;
+
+    public Freshman(string name, int course, double[] grades)
+    {
+        this.name = name;
+        this.course = course;
+        this.grades = grades;
+    }
+
+    public double CalculateAverageGrade()
+    {
+        if (grades.Length == 0)
+        {
+            return 0.0; // Если нет оценок, возвращаем 0
+        }
+
+        double sum = 0;
+        foreach (var grade in grades)
+        {
+            sum += grade;
+        }
+        return sum / grades.Length;
+    }
+
+    public int GetCourse()
+    {
+        return course;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
+
+// Класс "Студент второго курса"
+public class Sophomore : IStudent
+{
+    private string name;
+    private double[] grades;
+    private int course;
+
+    public Sophomore(string name, int course, double[] grades)
+    {
+        this.name = name;
+        this.course = course;
+        this.grades = grades;
+    }
+
+    public double CalculateAverageGrade()
+    {
+        if (grades.Length == 0)
+        {
+            return 0.0; // Если нет оценок, возвращаем 0
+        }
+
+        double sum = 0;
+        foreach (var grade in grades)
+        {
+            sum += grade;
+        }
+        return sum / grades.Length;
+    }
+
+    public int GetCourse()
+    {
+        return course;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество студентов: ");
+        if (!int.TryParse(Console.ReadLine(), out int studentCount) || studentCount <= 0)
+        {
+            Console.WriteLine("Ошибка! Введите корректное количество студентов.");
+            Console.ReadLine();
+            return;
+        }
+
+        List<IStudent> students = new List<IStudent>();
+
+        Console.WriteLine("Введите информацию о студентах:");
+        for (int i = 0; i < studentCount; i++)
+        {
+            Console.Write("Введите имя студента: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите курс студента (от 1 до 4): ");
+            if (!int.TryParse(Console.ReadLine(), out int course) || course < 1 || course > 4)
+            {
+                Console.WriteLine("Ошибка! Введите корректный курс (от 1 до 4).");
+                i--; // Возвращаемся на предыдущий шаг в цикле
+                continue;
+            }
+
+            Console.Write("Введите оценки студента (через пробел): ");
+            string gradesInput = Console.ReadLine();
+            string[] gradesArray = gradesInput.Split(' ');
+            List<double> gradesList = new List<double>();
+
+            foreach (var gradeStr in gradesArray)
+            {
+                if (double.TryParse(gradeStr, out double grade))
+                {
+                    gradesList.Add(grade);
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при вводе оценки: {gradeStr}. Пропускаем ее.");
+                }
+            }
+
+            IStudent student;
+            if (course == 1)
+            {
+                student = new Freshman(name, course, gradesList.ToArray());
+            }
+            else
+            {
+                student = new Sophomore(name, course, gradesList.ToArray());
+            }
+
+            students.Add(student);
+        }
+
+        Console.WriteLine("Информация о студентах:");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Имя: {student.GetName()}");
+            Console.WriteLine($"Курс: {student.GetCourse()}");
+            Console.WriteLine($"Средний балл: {student.CalculateAverageGrade():N2}");
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Интерфейс "Книга"
+public interface IBook
+{
+    bool IsAvailable();
+    void Borrow();
+    void Return();
+    string GetTitle();
+}
+
+// Класс "Книга в библиотеке"
+public class LibraryBook : IBook
+{
+    private string title;
+    private bool isBorrowed;
+
+    public LibraryBook(string title)
+    {
+        this.title = title;
+        isBorrowed = false;
+    }
+
+    public bool IsAvailable()
+    {
+        return !isBorrowed;
+    }
+
+    public void Borrow()
+    {
+        if (IsAvailable())
+        {
+            isBorrowed = true;
+            Console.WriteLine($"Книга \"{title}\" выдана.");
+        }
+        else
+        {
+            Console.WriteLine($"Книга \"{title}\" уже выдана.");
+        }
+    }
+
+    public void Return()
+    {
+        if (IsAvailable())
+        {
+            Console.WriteLine($"Книгу \"{title}\" нельзя вернуть, так как она не выдавалась.");
+        }
+        else
+        {
+            isBorrowed = false;
+            Console.WriteLine($"Книга \"{title}\" возвращена.");
+        }
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество книг: ");
+        if (!int.TryParse(Console.ReadLine(), out int bookCount) || bookCount <= 0)
+        {
+            Console.WriteLine("Ошибка! Введите корректное количество книг.");
+            Console.ReadLine();
+            return;
+        }
+
+        List<IBook> library = new List<IBook>();
+
+        Console.WriteLine("Введите информацию о книгах:");
+        for (int i = 0; i < bookCount; i++)
+        {
+            Console.Write("Введите название книги: ");
+            string input = Console.ReadLine();
+
+            IBook book = new LibraryBook(input);
+            library.Add(book);
+        }
+
+        Console.WriteLine("Список книг в библиотеке:");
+        foreach (var book in library)
+        {
+            Console.WriteLine($"Название: {book.GetTitle()}");
+            if (book.IsAvailable())
+            {
+                Console.WriteLine("Статус: доступна");
+            }
+            else
+            {
+                Console.WriteLine("Статус: выдана");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Желаете взять или вернуть книгу? (введите 'взять' или 'вернуть', или 'q' для выхода):");
+        while (true)
+        {
+            string action = Console.ReadLine();
+            if (action.ToLower() == "q")
+                break;
+
+            Console.Write("Введите название книги: ");
+            string bookTitle = Console.ReadLine();
+
+            IBook book = library.Find(b => b.GetTitle() == bookTitle);
+            if (book == null)
+            {
+                Console.WriteLine($"Книга с названием \"{bookTitle}\" не найдена.");
+                continue;
+            }
+
+            if (action.ToLower() == "взять")
+            {
+                book.Borrow();
+            }
+            else if (action.ToLower() == "вернуть")
+            {
+                book.Return();
+            }
+
+            Console.WriteLine("Желаете продолжить операции с книгами? (введите 'взять' или 'вернуть', или 'q' для выхода):");
+        }
+
+        Console.WriteLine("Завершение работы программы.");
+        Console.ReadLine();
+    }
+}
